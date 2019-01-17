3622d35686229a40ae43771e1e0cf3a05ee717a3
emad_ <emad_566@hotmail.com>
2019-01-17 03:53:11 +0200

    'removeAllFiles'

--- first_project01/first_app/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def index(request):
-    my_dict = {"insert_content": "I'm the inserted content"}
-    return render(request, "first_app/index.html", my_dict)
